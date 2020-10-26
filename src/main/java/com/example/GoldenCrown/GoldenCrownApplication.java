247588797d7eefa54f5d76877f1aac6c1fb22df9
Anoop-715 <66418745+Anoop-715@users.noreply.github.com>
2020-10-26 14:38:54 +0530

    Update GoldenCrownApplication.java

--- src/main/java/com/example/GoldenCrown/GoldenCrownApplication.java
+++ src/main/java/com/example/GoldenCrown/GoldenCrownApplication.java
@@ -13,17 +13,12 @@ public class GoldenCrownApplication {
 	public static void main(String[] args) throws IOException {
 
 		SpringApplication.run(GoldenCrownApplication.class, args);
-//		String kingdomMessage = "AIR ROZO";
+		List<String> kingdomMessages = new ArrayList<>();
+		String path ="C:\\Users\\DELL\\Downloads\\";
+		boolean isPathValid = FileInputReader.isPathValid(path) ;
+		if(isPathValid) { kingdomMessages = FileInputReader.fileReader(path); }
+		else { System.out.print("Path given is wrong "); }
 
-		//read the message from a file
-		// param to a static method in the file reader file location
-
-		String path ="C:\\Users\\DELL\\Downloads\\GreekTrust_Input.txt ";
-		List<String> kingdomMessages =  FileInputReader.fileReader(path);
-
-
-
-//		List<String> kingdomMessages = List.of("AIR ROZO", "LAND FAIJWJSOOFAMAU");
 		Set<Kingdom> supportedKingdoms = GoldenCrownService.getSupportedKingdoms(kingdomMessages);
 		if(supportedKingdoms.size()>0){
 			System.out.print(Kingdom.SPACE.name()+ " ");
