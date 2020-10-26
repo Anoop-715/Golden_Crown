dd1e44266f9cf68e807e29413836aa3315f9fc42
Anoop-715 <anoop.a715@gmail.com>
2020-10-26 15:08:21 +0530

    File Directory solved

--- src/main/java/com/example/GoldenCrown/FileInputReader.java
+++ src/main/java/com/example/GoldenCrown/FileInputReader.java
@@ -1,9 +1,6 @@
 package com.example.GoldenCrown;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
