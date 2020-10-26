ce1c3ac3bb009864460a2b1032e011ebf2cc959c
Anoop-715 <anoop.a715@gmail.com>
2020-10-26 11:43:06 +0530

    File Reader

--- src/main/java/com/example/GoldenCrown/FileReader.java
+++ src/main/java/com/example/GoldenCrown/FileReader.java
@@ -1,2 +1,29 @@
-package com.example.GoldenCrown;public class FileReader {
+package com.example.GoldenCrown;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+
+    public static List<String> fileReader(String path) throws IOException {
+        List<String> kingdomMessages = new ArrayList<>();
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader(path));
+            String line = bufReader.readLine();
+            try {
+                while (line != null) {
+                    kingdomMessages.add(line);
+                    line = bufReader.readLine();
+                }
+            } finally {
+                bufReader.close();
+            }
+        }  catch (IOException e) {
+            System.out.print(e);
+        }
+        return kingdomMessages;
+    }
 }
